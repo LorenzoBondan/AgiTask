622e81f15cbf6caa8e3247e24a845ed0e7757396
LorenzoBondan <lbbondan@gmail.com>
2023-06-15 22:41:09 -0300

    companies that are already using

---
+++ frontend/src/Components/Companies/index.tsx
@@ -0,0 +1,85 @@
+
+import './styles.css';
+
+const Companies = () => {
+
+    const companies = [
+        {
+            name: "Apple",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/391px-Apple_logo_black.svg.png"
+        },
+        {
+            name: "Microsoft",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png"
+        },
+        {
+            name: "BMW",
+            imgUrl: "https://vetores.org/d/bmw.svg"
+        },
+        {
+            name: "CNN",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/CNN_International_logo.svg/2048px-CNN_International_logo.svg.png"
+        },
+        {
+            name: "Discovery",
+            imgUrl: "https://logodownload.org/wp-content/uploads/2017/04/discovery-channel-logo-0.png"
+        },
+        {
+            name: "Land Rover",
+            imgUrl: "https://www.imagensempng.com.br/wp-content/uploads/2020/12/0029.png"
+        },
+        {
+            name: "Coca-Cola",
+            imgUrl: "https://logospng.org/download/coca-cola/logo-coca-cola-1536.png"
+        },
+        {
+            name: "Grêmio",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Gremio_logo.gif"
+        },
+        {
+            name: "Mc Donalds",
+            imgUrl: "https://www.mcdonalds.com.br/images/layout/mcdonalds-logo-bg-red.png"
+        },
+        {
+            name: "Dell",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Dell_logo_2016.svg/800px-Dell_logo_2016.svg.png"
+        },
+        {
+            name: "Amazon",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/2500px-Amazon_icon.svg.png"
+        },
+        {
+            name: "Jeep",
+            imgUrl: "https://logowik.com/content/uploads/images/jeep.jpg"
+        },
+        {
+            name: "Nivea",
+            imgUrl: "https://logodownload.org/wp-content/uploads/2017/03/nivea-logo-1-1.png"
+        },
+        {
+            name: "Adidas",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1280px-Adidas_Logo.svg.png"
+        },
+        {
+            name: "Netflix",
+            imgUrl: "https://i.pinimg.com/originals/da/f3/0f/daf30fac5e16393d66a3684dd27e29af.png"
+        },
+
+    ]
+    return(
+        <div className='companies-container'>
+            <h3>Companies that are already using AgiTask</h3>
+            <div className='row companies-row'>
+                {companies.map(company => (
+                    <div className="col-sm-12 col-lg-4 col-xl-2 utilized-column">
+                    <div className='company base-card'>
+                        <img src={company.imgUrl} alt="" />
+                    </div> 
+                </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Companies;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@
 import Banner from 'Components/Banner';
 import './styles.css';
 import Utilized from 'Components/Utilized';
+import Companies from 'Components/Companies';
 
 const Home = () => {
     return(
@@ -10,6 +11,7 @@ const Home = () => {
             <div className='home-team-united'>
                 <div className='home-team-text'>
                     <h3>Your team united as never before</h3>
+                    <p className='separator'></p>
                     <p>AgiTask offers a single plan with all features included to your company grow up!</p>
                     <p style={{fontStyle:"italic"}}>Our team worked hard. There were sleepless nights and lots of coffee. <br /> All of this to deliver a solution that allows not only to serve clients but also to manage projects <br />in a simple and intuitive way, making teams more engajed and productive.</p>
                 </div>
@@ -19,6 +21,9 @@ const Home = () => {
             </div>
             <div className='home-utilized-container'>
                 <Utilized/>
+            </div>
+            <div className='home-utilized-container'>
+                <Companies/>
             </div>
         </div>
     );
